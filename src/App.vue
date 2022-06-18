9865497eba9dba3e448e416d41b2c0ecfd3d29e7
Deiminas <70855330+TheyMiss@users.noreply.github.com>
2022-06-18 22:36:45 +0300

    Added Block component and some styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
-<template>Hello World!</template>
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="startGame()" :disabled="isPlaying">Start Game</button>
+  <BlockComp v-if="isPlaying" :delay="delay" />
+</template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import BlockComp from "./components/Block-comp.vue";
 
 export default defineComponent({
   name: "App",
+  components: { BlockComp },
+  setup() {
+    const isPlaying = ref(false);
+    const delay = ref<null | number>(null);
+
+    const startGame = () => {
+      isPlaying.value = true;
+      delay.value = 2000 + Math.random() * 5000;
+    };
+
+    return { isPlaying, delay, startGame };
+  },
 });
 </script>
 
@@ -13,5 +30,24 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   margin-top: 60px;
+}
+
+button {
+  all: unset;
+  background: rgb(21, 145, 161);
+  padding: 1rem;
+  color: white;
+  border-radius: 0.3rem;
+  cursor: pointer;
+}
+
+button:hover {
+  background: rgb(12, 98, 109);
+}
+
+button:disabled,
+button[disabled] {
+  background-color: rgb(25, 41, 46);
+  color: white;
 }
 </style>
---
+++ src/components/Block-comp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block">Block</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    delay: {
+      validator: (props) => typeof props === "string" || props === null,
+      required: false,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.block {
+  margin: 40px auto;
+  background: rgb(26, 182, 148);
+  width: 400px;
+  padding: 100px 0;
+  text-align: center;
+  color: white;
+  border-radius: 0.5rem;
+}
+</style>
