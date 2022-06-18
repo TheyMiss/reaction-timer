3725f3486b0a0b43664d523d67f45661e3945c2b
Deiminas <70855330+TheyMiss@users.noreply.github.com>
2022-06-18 21:46:25 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>Hello World!</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
